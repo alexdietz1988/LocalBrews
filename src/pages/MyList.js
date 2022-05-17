f205dedce69dcf90991de7ec060ca8a5b511d227
alexdietz1988 <alex.dietz@gmail.com>
2022-05-17 19:32:26 -0400

    MyList displays data from database

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -3,51 +3,48 @@ import { Link } from "react-router-dom"
 
 function MyList(props) {
 
-
     function getMyList() {
         fetch(`http://localhost:4000/my-list/${props.user}`)
             .then(response => response.json())
-            props.setUserList((prevState) => ({
-                ...prevState,
-                [e.target.name]: e.target.value
-            }))
-            .then(data => console.log(data))
-            // .then(data => props.setUserList(data))
-            // .then(console.log(props.userList))
+            .then(data => props.setUserList(data))
     }
 
     useEffect(() => {getMyList()}, [])
 
-    // function handleSubmit(e) {
-    //     e.preventDefault()
-    //     let userList = props.userList
-    //     let index = userList.findIndex(element => element.id = id)
-    //     userList.splice(index, 1)
-    //     props.setUserList(userList)
-    // }
+    function handleSubmit(e) {
+        e.preventDefault()
+        // let userList = props.userList
+        // let index = userList.findIndex(element => element.id = id)
+        // userList.splice(index, 1)
+        // props.setUserList(userList)
+    }
 
-    // function mapMyList() {
-    //     const mapping = myList.map(brewery => {
-    //         return(
-    //             <div>
-    //                 <Link className='d-inline-block' to={`/brewery/${brewery.id}`} key={brewery.id}><p>{brewery.name}</p></Link>
-    
-    //                 {/* <form className='d-inline-block p-4' onSubmit={handleSubmit}>
-    //                     <button className='btn btn-warning'>Remove</button>
-    //                 </form> */}
-    //             </div>)
-    //     })
-    //     return mapping
-    // }
+    function loaded() {
 
-    // if (myList) mapMyList()
+        return(
+            <section>
+                <h3 className='mb-3'>Results</h3>
+                {props.userList.map(brewery => (
 
-    // return(
-    //     <>
-    //         <h2 className='mb-4'>My List</h2>
-    //         {myList ? mapMyList() : <p>Loading...</p>}
-    //     </>
-    // )
+                    <div>
+                    <Link className='d-inline-block' to={`/brewery/${brewery.brewery_id}`} key={brewery.brewery_id}><p>{brewery.name}, {brewery.location}</p></Link>
+                    <form className='d-inline-block p-4' onSubmit={handleSubmit}>
+                        <button className='btn btn-warning'>Remove</button>
+                    </form>
+                </div>
+
+                ))}
+            </section>
+        )
+    }
+
+    return(
+        <>
+        <h2 className='mb-4'>My List</h2>
+        {props.userList ? loaded() : <p>Loading...</p>}
+        </>
+    )
+
 }
 
 export default MyList
