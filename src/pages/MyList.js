b72f57aaa7880f016012967efdb2a392ea20a767
alexdietz1988 <alex.dietz@gmail.com>
2022-05-17 11:45:52 -0400

    Add remove button to My List

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -2,11 +2,28 @@ import { Link } from "react-router-dom"
 
 function MyList(props) {
 
-    const mapping = props.userList.map(brewery => {
-        return <Link to={`/brewery/${brewery.id}`} key={brewery.id}><p>{brewery.name}</p></Link>
-    })
+    let id
 
-    console.log(props.userList)
+    function handleSubmit(e) {
+        e.preventDefault()
+        let userList = props.userList
+        let index = userList.findIndex(element => element.id = id)
+        userList.splice(index, 1)
+        props.setUserList(userList)
+    }
+
+    const mapping = props.userList.map(brewery => {
+        id = brewery.id
+
+        return(
+            <div>
+                <Link className='d-inline-block' to={`/brewery/${brewery.id}`} key={brewery.id}><p>{brewery.name}</p></Link>
+
+                <form className='d-inline-block p-4' onSubmit={handleSubmit}>
+                    <button className='btn btn-warning'>Remove</button>
+                </form>
+            </div>)
+    })
 
     return(
         <>
