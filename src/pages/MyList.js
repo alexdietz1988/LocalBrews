7f4f02ac91271a6a97cb6a1d1ca5f555119f1ad6
alexdietz1988 <alex.dietz@gmail.com>
2022-05-18 08:42:48 -0400

    Remove 'remove brewery' button from My List

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -11,28 +11,12 @@ function MyList(props) {
 
     useEffect(() => {getMyList()}, [])
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        // let userList = props.userList
-        // let index = userList.findIndex(element => element.id = id)
-        // userList.splice(index, 1)
-        // props.setUserList(userList)
-    }
-
     function loaded() {
-
+        console.log(props.userList)
         return(
             <section>
-                <h3 className='mb-3'>Results</h3>
                 {props.userList.map(brewery => (
-
-                    <div>
-                    <Link className='d-inline-block' to={`/brewery/${brewery.brewery_id}`} key={brewery.brewery_id}><p>{brewery.name}, {brewery.location}</p></Link>
-                    <form className='d-inline-block p-4' onSubmit={handleSubmit}>
-                        <button className='btn btn-warning'>Remove</button>
-                    </form>
-                </div>
-
+                    <Link to={`/brewery/${brewery.brewery_id}`} key={brewery.brewery_id}><p>{brewery.name}, {brewery.location}</p></Link>
                 ))}
             </section>
         )
