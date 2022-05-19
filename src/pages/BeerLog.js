f5fd076267a2479754b1c5978348436267f200d5
alexdietz1988 <alex.dietz@gmail.com>
2022-05-19 09:15:25 -0400

    Delete beer

--- src/pages/BeerLog.js
+++ src/pages/BeerLog.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 function BeerLog(props) {
     const [beerLog, setBeerLog] = useState([])
@@ -8,23 +9,33 @@ function BeerLog(props) {
         fetch(`http://localhost:4000/logs/beer-log/${props.username}/`)
             .then(response => response.json())
             .then(data => {
+                console.log(data)
                 setBeerLog(data)
             })
     }
 
     useEffect(() => {getBeerLog()}, [])
 
+    function removeBeer(e) {
+        e.preventDefault()
+        axios.delete(`http://localhost:4000/logs/beer/${e.target.name}`)
+    }
+
     function loaded() {
         return(
             <section>
                 {beerLog.map(beer => (
-                    <div key={beer.id}>
+                    <div key={beer._id}>
                         <p>
                             <b>{beer.name}</b><br />
                             <i>Brewery:</i> <Link to={`/brewery/${beer.brewery_id}`}>{beer.brewery_name}</Link>, {beer.brewery_location}<br />
                             <i>Style:</i> {beer.style}<br />
                             <i>Your Rating:</i> {beer.rating}
                         </p>
+
+                        <form name={beer._id} onSubmit={removeBeer}>
+                            <button className="btn btn-warning">Remove Beer</button>
+                        </form>
                     </div>
                 ))}
             </section>
