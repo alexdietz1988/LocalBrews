d397938e064190124feda5bd30da2740f951a47e
alexdietz1988 <alex.dietz@gmail.com>
2022-06-16 09:39:03 -0400

    Finish signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,37 @@
+import axios from "axios"
+import useState from "react"
+
 function SignUp() {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+
+    function handleChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        axios.post(props.backend + 'logs/beer', {
+            username: formData.username,
+            password: formData.password
+        })
+    }
+
     return(
         <>
             <h4 className='mb-4'>Sign Up</h4>
-            <form className='mb-4 row g-1'>
+            <form className='mb-4 row g-1' onSubmit={handleSubmit}>
                 <div className='mb-2 col-sm'>
-                    <input placeholder='Username' name='username' type="text" className='form-control' required />
+                    <input placeholder='Username' name='username' type="text" className='form-control' onChange={handleChange} required />
                 </div>
                 <div className='mb-2 col-sm'>
-                    <input placeholder='Password' type="password" className='form-control' required />
+                    <input placeholder='Password' type="password" className='form-control' onChange={handleChange} required />
                 </div>
                 <div className='mb-2 col-sm'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
@@ -18,31 +42,3 @@ function SignUp() {
 }
 
 export default SignUp
-
-
-{/* <h4 className='mb-4'>Log a Beer from This Brewery</h4>
-<form className='mb-4 row g-1' onSubmit={props.handleSubmit}>
-
-    <div className='mb-2 col-sm'>
-    <input placeholder='Beer Name'  name='name' type='text' className='form-control' value={props.beer.name} onChange={props.handleChange}></input>
-    </div>
-
-    <div className='mb-2 col-sm'>
-    <input placeholder='Beer Style'  name='style' type='text' className='form-control' value={props.beer.style} onChange={props.handleChange}></input>
-    </div>
-
-    <div className='mb-2 col-sm'>
-    <select placeholder='Rating' id='rating' name='rating' className='form-select' value={props.beer.rating} onChange={props.handleChange}>
-        <option>Rating</option>
-        <option value='5'>5</option>
-        <option value='4'>4</option>
-        <option value='3'>3</option>
-        <option value='2'>2</option>
-        <option value='1'>1</option>
-    </select>
-    </div>
-
-    <div className='mb-2 col-sm'>
-    <button type='submit' className='btn btn-primary'>Submit</button>
-    </div>
-</form> */}
