4d91a3bae530187d061c37004bcbb90fe1b8a3a0
alexdietz1988 <alex.dietz@gmail.com>
2022-08-10 10:24:27 -0400

    Debug brewery search

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import axios from "axios"
 import SearchForm from "./SearchForm"
 import SearchResults from "./SearchResults"
 
@@ -6,12 +7,17 @@ function Search(props) {
 
     const [breweries, setBreweries] = useState([])
     const [location, setLocation] = useState({city: '', state: ''})
-    const [search, setSearch] = useState(false)
 
     function searchByCity() {
-        fetch(props.openBrewery + `?by_city=${location.city}&by_state=${location.state}&per_page=50`)
-            .then(response => response.json())
-            .then(data => {setBreweries(data)})
+        axios.get(props.openBrewery, {
+            params: {
+                by_city: location.city,
+                by_state: location.state,
+                per_page: 50
+            }
+        })
+            .then(({ data }) => setBreweries(data))
+            .catch((error) => console.log(error))
     }
 
     function handleChange(e) {
@@ -24,14 +30,13 @@ function Search(props) {
     function handleSubmit(e) {
         e.preventDefault()
         searchByCity()
-        setSearch(true)
     }
 
     return(
         <>
             <h2 className='mb-4'>Brewery Search</h2>
             <SearchForm location={location} handleChange={handleChange} handleSubmit={handleSubmit}/>
-            {search ? <SearchResults breweries={breweries}/> : null}
+            {breweries.length > 0 ? <SearchResults breweries={breweries}/> : null}
         </>
     )
 }
