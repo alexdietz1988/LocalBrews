224d6e0680f7a358e89203ad745e46e1f371211c
alexdietz1988 <alex.dietz@gmail.com>
2022-08-10 10:18:12 -0400

    Refactor brewery search

--- src/pages/beerlog/BeerLog.js
+++ src/pages/beerlog/BeerLog.js
@@ -14,7 +14,6 @@ function BeerLog(props) {
     useEffect(() => getBeerLog(), [])
 
     function removeBeer(id) {
-        e.preventDefault()
         axios.delete(props.backend + 'logs/beer/' + id)
             .then(() => getBeerLog())
             .catch((error) => console.log(error))
