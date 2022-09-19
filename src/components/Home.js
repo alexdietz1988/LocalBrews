fe08ef0de5e5de66901e071cd42d985b0705e838
alexdietz1988 <alex.dietz@gmail.com>
2022-09-19 11:30:28 -0400

    Refactor home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,30 @@
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-function Home({user, newUser, logout}) {
-
-    if (!user && logout) {
-        return (
-            <div className='home'>
-                <h4 className='mb-4'>Logout Successful</h4>
-                <p>Come again soon!</p>
-            </div>
-        )
-    }
-
-    else if (!user) {
-        return(
-            <div className='home'>
-                <h2 className='mb-3'>Welcome to Local Brews!</h2>
-
-                <section>
+function Home(props) {
+    return(
+        <div className='home'>
+            <h4 className='mb-4'>Welcome to Local Brews!</h4>
+            <section>
+            {props.isSignedIn ?
+                <>
+                    <p>Try <Link to='/search'>searching for a brewery to add to your list</Link>.</p>
+                    <p>Or see what <Link to='/breweries'>breweries</Link> and <Link to='/beers'>beers</Link> you've saved so far.</p>
+                </> :
+                <>
                     <p>Like trying new beers from local breweries?</p>
                     <p>Try <Link to='/search'>searching for a brewery near you</Link>.</p>
                     <p><Link to='/signup'>Sign up</Link> or <Link to='/login'>log in</Link> to save your favorite local breweries and beers.</p>
-                </section>
-            </div>
-        )
-    }
-
-    let welcome = newUser ? 'Welcome to Local Brews' : 'Welcome back'
-
-    return(
-        <div className='home'>
-            <h4 className='mb-4'>{welcome}, {user}!</h4>
-            <p>Try <Link to='/search'>searching for a brewery to add to your list</Link>.</p>
-            <p>Or see what <Link to='/mylist'>breweries</Link> and <Link to='/beer-log'>beers</Link> you've saved so far.</p>
+                </>
+            }
+            </section>
         </div>
     )
 }
 
 function mapStateToProps(state) {
     return {
-        user: state.user
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
